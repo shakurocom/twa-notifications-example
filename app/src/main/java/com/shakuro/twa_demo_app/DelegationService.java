da2b7d0c007fca0315d2bcbff9bd30a999ae6184
Konstantin Bozhkov <enterkostya@mail.ru>
2021-01-27 17:50:35 +0300

    Add example

---
+++ app/src/main/java/com/shakuro/twa_demo_app/DelegationService.java
@@ -0,0 +1,9 @@
+package com.shakuro.twa_demo_app;
+
+public class DelegationService extends com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
+
